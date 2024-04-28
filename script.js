4773dfcad72bd7a258416a1911faf3e0339fe97d
Costin Lotreanu <costin.lotreanu@gmail.com>
2024-04-28 17:52:57 +0300

    add event listener

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// select elements
+const quote = document.querySelector('#quote');
+const author = document.querySelector('#author');
+const btnTwitter = document.querySelector('#twitter');
+const btnNewQuote = document.querySelector('#new-quote');
+
+// onload
+getQuote();
+
 // getQuote API
 async function getQuote() {
   const proxyUrl = 'https://proxy.cors.sh/';
@@ -6,12 +15,26 @@ async function getQuote() {
   try {
     const response = await fetch(proxyUrl + APIurl);
     const data = await response.json();
-    console.log(data);
+    // Unknow author if no value
+    data.quoteAuthor = data.quoteAuthor ? data.quoteAuthor : 'Unknown';
+
+    // Show quote
+    quote.innerText = data.quoteText;
+    author.innerText = data.quoteAuthor;
   } catch (error) {
     getQuote();
     console.log(error);
   }
 }
 
-// onload
-getQuote();
+// Tweet (X platform now)
+function tweet() {
+  const textTweet = quote.innerText;
+  const authorTweet = author.innerText;
+  const url = `https://twitter.com/intent/tweet?text=${textTweet} - ${authorTweet}`;
+  window.open(url, '_blank');
+}
+
+// event listener
+btnNewQuote.addEventListener('click', getQuote);
+btnTwitter.addEventListener('click', tweet);
